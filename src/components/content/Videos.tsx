5c0c133608d29f08a0bf91f16a53ee4ec154e2c7
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-01 19:40:07 +0200

    Update: Change title in Videos.tsx

--- src/components/content/Videos.tsx
+++ src/components/content/Videos.tsx
@@ -1,10 +1,19 @@
+import icons from '@/lib/constants/icons';
+import Image from 'next/image';
+import { Volume2 } from 'lucide-react';
+import { SquarePlay } from 'lucide-react';
+
 export default function Videos({ src }: { src: string }) {
 	return (
 		<div className="w-full flex justify-center">
 			<section className="w-full max-w-5xl text-center mx-auto">
-				<p className="text-zinc-600 mt-4 font-normal text-center text-lg px-10">
-					Video Preview
-				</p>
+				<div className="flex flex-row items-center justify-center mt-4 px-10 gap-2">
+					<p className="text-zinc-600 font-normal text-center text-lg">
+						Video Preview
+					</p>
+					<Volume2 color='#525252'/>
+					<SquarePlay color='#525252'/>
+				</div>
 				<div className="py-3">
 					<div className="w-full max-w-[360px] aspect-[9/16] mx-auto border border-gray-200 rounded-2xl overflow-hidden shadow-md">
 						<video controls className="w-full h-full object-contain">
--- src/lib/constants/icons/index.ts
+++ src/lib/constants/icons/index.ts
@@ -1,5 +1,7 @@
 const icons = {
 	mind_map: '/icons/mind-map.png',
+	sound: '/icons/sound.png',
+	video: '/icons/video.png',
 };
 
 export default icons;
