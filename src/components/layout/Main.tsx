02368e18e253ce67fdf14b7f00265a0660184cf1
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-03 11:15:10 +0200

    Styles: Change bg color in Main.tsx

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,6 +1,6 @@
 export default function Main({ children }: { children: React.ReactNode }) {
 	return (
-		<main className="mx-auto w-full grow bg-zinc-100">
+		<main className="mx-auto w-full grow bg-background">
 			{children}
 		</main>
 	)
