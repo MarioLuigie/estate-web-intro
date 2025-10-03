f140e695e4b15248ed45b872c29685e8425b8687
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-03 10:47:33 +0200

    Update: Change file version for video in /constants/videos/index.ts

--- src/lib/constants/videos/index.ts
+++ src/lib/constants/videos/index.ts
@@ -1,5 +1,5 @@
 const videos = {
-	estate_video: 'https://estate-app-767397703010.s3.us-east-1.amazonaws.com/Estate_16.mp4',
+	estate_video: 'https://estate-app-767397703010.s3.us-east-1.amazonaws.com/Estate_17.mp4',
 };
 
 export default videos;
