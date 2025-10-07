31818acdaa8a5deddcfb2da7320128c684e911f2
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-07 16:52:06 +0200

    Feat: Create CustomAccordion.tsx

--- src/components/content/AppFlow.tsx
+++ src/components/content/AppFlow.tsx
@@ -1,9 +1,10 @@
 import icons from '@/lib/constants/icons';
 import Image from 'next/image';
+import CustomAccordion from '@/components/shared/CustomAccordion';
 
 export default function AppFlow() {
 	return (
-		<div className="w-full flex flex-col items-center pt-16">
+		<div className="w-full max-w-5xl mx-auto flex flex-col items-center pt-16 px-6">
 			<p className="text-center font-semibold">App Flow | mind map in development.</p>
 			<p className="text-center px-6">
 				Represents the application architecture, component interactions, and
@@ -11,6 +12,9 @@ export default function AppFlow() {
 			</p>
 			<div className='mt-8'>
 				<Image src={icons.mind_map} alt="Icon" width={100} height={100} />
+			</div>
+			<div className='w-full'>
+				<CustomAccordion />
 			</div>
 		</div>
 	);
---
+++ src/lib/constants/images/index.ts
@@ -0,0 +1,7 @@
+const images = {
+	c1_model: '/images/c1.png',
+	c2_model: '/images/c2.png',
+	c3_model: '/images/c3.png',
+};
+
+export default images;
