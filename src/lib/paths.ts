bafd80fbc49a073ae616d074452846ba25ebe51d
Mariusz Lotocki <mk.lotocki@gmail.com>
2025-10-08 14:35:36 +0200

    Update: Add agilable path for embeded item in canvaUrls in paths.ts

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -6,5 +6,5 @@ export const canvaUrls = {
 	c3_url:
 		'https://www.canva.com/design/DAG09P0ooaw/WA55H_3VJf6DWzlDk0mjzw/view?embed',
 	c4_url:
-		'https://www.canva.com/design/DAG08oeQi8w/TzSJw8bLM0w4H7lo8WANPg/view?embed',
+		'https://www.canva.com/design/DAG1MKPNBvI/8ozEQvitgHIX7ZPYILluug/view?embed',
 };
